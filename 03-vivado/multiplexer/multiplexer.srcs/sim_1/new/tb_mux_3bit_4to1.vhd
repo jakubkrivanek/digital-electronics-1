b636d9d9e82584d785d76921905b12224717ac01
jakubkrivanek <krivan.jakub@seznam.cz>
2023-02-21 14:46:16 +0100

    03-vivado

---
+++ 03-vivado/multiplexer/multiplexer.srcs/sim_1/new/tb_mux_3bit_4to1.vhd
@@ -0,0 +1,61 @@
+------------------------------------------------------------
+--
+-- Testbench for 2-bit binary comparator.
+-- EDA Playground
+--
+-- Copyright (c) 2020 Tomas Fryza
+-- Dept. of Radio Electronics, Brno Univ. of Technology, Czechia
+-- This work is licensed under the terms of the MIT license.
+--
+------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+------------------------------------------------------------
+-- Entity declaration for test bench
+------------------------------------------------------------
+entity tb_mux_3bit_4to1 is
+    -- Entity of testbench is always empty
+end entity tb_mux_3bit_4to1;
+
+------------------------------------------------------------
+-- Architecture body for testbench
+------------------------------------------------------------
+architecture testbench of tb_mux_3bit_4to1 is
+
+    -- Local signals
+        a_i           : in  std_logic_vector(3 - 1 downto 0);
+        b_i           : in  std_logic_vector(3 - 1 downto 0); -- COMPLETE THE ENTITY DECLARATION
+        c_i           : in  std_logic_vector(3 - 1 downto 0);
+        d_i           : in  std_logic_vector(3 - 1 downto 0);
+        sel_i         : in  std_logic_vector(2 - 1 downto 0);
+        f_o           : out std_logic_vector(3 - 1 downto 0)
+
+begin
+    -- Connecting testbench signals with comparator_2bit
+    -- entity (Unit Under Test)
+    uut_mux_3bit_4to1 : entity work.mux_3bit_4to1
+        port map(
+            a_i           => s_a,
+            b_i           => s_b,
+            B_greater_A_o => s_B_greater_A,
+            B_equals_A_o  => s_B_equals_A,
+            B_less_A_o    => s_B_less_A
+        );
+
+    --------------------------------------------------------
+    -- Data generation process
+    --------------------------------------------------------
+p_label : process (a)
+begin
+    if (a = '0') then
+        q <= '0';
+    else
+        q <= '1';
+    end if;
+end process p_label;
+
+    end process p_stimulus;
+    
+end architecture testbench;
---
+++ 03-vivado/multiplexer/multiplexer.srcs/sources_1/new/mux_3bit_4to1.vhd
@@ -0,0 +1,38 @@
+-- assignments.
+-- EDA Playground
+--
+-- Copyright (c) 2020 Tomas Fryza
+-- Dept. of Radio Electronics, Brno Univ. of Technology, Czechia
+-- This work is licensed under the terms of the MIT license.
+--
+------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+------------------------------------------------------------
+-- Entity declaration for 2-bit binary comparator
+------------------------------------------------------------
+entity mux_3bit_4to1 is
+    port(
+        a_i           : in  std_logic_vector(3 - 1 downto 0);
+        b_i           : in  std_logic_vector(3 - 1 downto 0);-- COMPLETE THE ENTITY DECLARATION
+        c_i           : in  std_logic_vector(3 - 1 downto 0);
+        d_i           : in  std_logic_vector(3 - 1 downto 0);
+        sel_i         : in  std_logic_vector(2 - 1 downto 0);
+        f_o           : out std_logic_vector(3 - 1 downto 0)
+        );
+end entity mux_3bit_4to1;
+
+------------------------------------------------------------
+-- Architecture body for 2-bit binary comparator
+------------------------------------------------------------
+architecture Behavioral of mux_3bit_4to1 is
+begin
+with sel_i select
+    f_o <= a_i when "00",  -- If addr_i = "000" then y_o = a_i
+           b_i when "01",
+           c_i when "10",
+           d_i when others; -- All other combinations
+         
+end architecture Behavioral;
